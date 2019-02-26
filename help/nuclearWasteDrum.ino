fda8d5605a98fd1d04ce18e7fe85673ddb53698f
cheapjack <ross@cheapjack.org.uk>
2019-02-26 11:52:10 +0000

    Tidied code with a proper ping() function that returns distance

--- help/nuclearWasteDrum.ino
+++ help/nuclearWasteDrum.ino
@@ -43,46 +43,52 @@ Adafruit_VS1053_FilePlayer musicPlayer =
   //Adafruit_VS1053_FilePlayer(BREAKOUT_RESET, BREAKOUT_CS, BREAKOUT_DCS, DREQ, CARDCS);
   // create shield-example object!
   Adafruit_VS1053_FilePlayer(SHIELD_RESET, SHIELD_CS, SHIELD_DCS, DREQ, CARDCS);
-////
-const int analogPin = A0;    // pin that the sensor is attached to
-// so make sure the
-const int pingPin = 5;
+
+// Setup our variables
+
+const int pingPin = 5; //pin that triggers a ping and reads it back on the same pin
 const int ledPin = 13;       // pin that the LED is attached to
-const int threshold = 30;   // an arbitrary threshold level that's in the range of the analog input
-const int startVolume = 20;   // an arbitrary threshold level that's in the range of the analog input
+const int startVolume = 10;   // an arbitrary volume level to start with, with VS1053 smaller numbers are louder volumes!
+const int threshold = 20; // arbitrary distance threshold to cross to play a file 
+
+// Setup and print some helpful debugging with Serial
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Adafruit VS1053 Library Test");
- 
-  
+  digitalWrite(ledPin, LOW);
   // initialise the music player
   if (! musicPlayer.begin()) { // initialise the music player
-     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
-     while (1);
+     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?")); // Throw error if musicPlayer fails to begin
+     while (1); // keep the error while its True ie 1
   }
-  Serial.println(F("VS1053 found"));
-
+  
+  Serial.println(F("VS1053 found")); // 's ok working
   musicPlayer.sineTest(0x44, 500);    // Make a tone to indicate VS1053 is working
- 
+  digitalWrite(ledPin, HIGH);
+  delay(100;
+  digitalWrite(ledPin, LOW);
+  
   if (!SD.begin(CARDCS)) {
     Serial.println(F("SD failed, or not present"));
-    while (1);  // don't do anything more
+    while (1);  // don't do anything more while theres no SD present
   }
-  Serial.println("SD OK!");
+  
+  Serial.println("SD OK!"); // yay it works!
   Serial.println();
   // list files
   printDirectory(SD.open("/"), 0);
-  
   // Set volume for left, right channels. lower numbers == louder volume!
   musicPlayer.setVolume(startVolume,startVolume);
 
-  /***** Two interrupt options! *******/ 
+  // Two interrupt options! // 
+  // Interrupt just means we can have a file playing but do something else, but with a minor cost.
+  // OPTION 1
   // This option uses timer0, this means timer1 & t2 are not required
   // (so you can use 'em for Servos, etc) BUT millis() can lose time
   // since we're hitchhiking on top of the millis() tracker
   //musicPlayer.useInterrupt(VS1053_FILEPLAYER_TIMER0_INT);
-  
+  // OPTION 2
   // This option uses a pin interrupt. No timers required! But DREQ
   // must be on an interrupt pin. For Uno/Duemilanove/Diecimilla
   // that's Digital #2 or #3
@@ -92,6 +98,9 @@ void setup() {
     Serial.println(F("DREQ pin is not an interrupt pin"));
 }
 
+// this is a function that returns a long variable (a number with lots of floating points) and not an int.
+// If we want to compare to an int we can convert later with the native int(x) function
+
 long microsecondsToCentimeters(long microseconds) {
   // The speed of sound is 340 m/s or 29 microseconds per centimeter.
   // The ping travels out and back, so to find the distance of the
@@ -99,55 +108,14 @@ long microsecondsToCentimeters(long microseconds) {
   return microseconds / 29 / 2;
 }
 
-void loop() {
-   // establish variables for duration of the ping,
-  // and the distance result in inches and centimeters:
-  long duration, cm;
+// Make a function to keep our PING code tidy. It's not a void function like void loop() because we want to return something, in this case distance.
 
-  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  pinMode(pingPin, OUTPUT);
-  digitalWrite(pingPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(pingPin, LOW);
-
-  // The same pin is used to read the signal from the PING))): a HIGH
-  // pulse whose duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  pinMode(pingPin, INPUT);
-  duration = pulseIn(pingPin, HIGH);
-
-  // convert the time into a distance
-  cm = microsecondsToCentimeters(duration);
-  Serial.println();
-  Serial.print("Distance from first ping = ");
-  Serial.print(cm);
-  Serial.print(" cm");
-  Serial.println();
-// delay for stability
-  delay(100);
-
-  // if the cm value is high enough, turn on the LED and playMusic()
-  if (cm > threshold) {
-    digitalWrite(ledPin, HIGH);
-    Serial.print("Distance over threshold so play music")
-    Serial.println();
-    playMusic();
-    // dont bother otherwise set LED to off
-  } else {
-    Serial.print("You're not close enough! So no play");
-    digitalWrite(ledPin, LOW);
-  }
-
-  delay(1);        // delay in between reads for stability
-
-  
-}
-
-void ping() {
-    // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
+long ping() {
+    // establish variables for duration of the ping,
+    // and the distance result in centimeters:
+    long duration, cm;
+    int distance = 0;
+    // The PING is triggered by a HIGH pulse OUT of 2 or more microseconds.
     // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
     pinMode(pingPin, OUTPUT);
     digitalWrite(pingPin, LOW);
@@ -155,59 +123,74 @@ void ping() {
     digitalWrite(pingPin, HIGH);
     delayMicroseconds(5);
     digitalWrite(pingPin, LOW);
-    // The same pin is used to read the signal from the PING))): a HIGH
+    // The same pin is used to read the signal IN from the PING: a HIGH
     // pulse whose duration is the time (in microseconds) from the sending
     // of the ping to the reception of its echo off of an object.
     pinMode(pingPin, INPUT);
     duration = pulseIn(pingPin, HIGH);
-    // convert the time into a distance
+    // convert the time into a distance with our function above; you can call a function
+    // within another function
     cm = microsecondsToCentimeters(duration);
-    return cm;
+    // Print to serial for info
     Serial.println();
     Serial.print("Distance from first ping = ");
     Serial.print(cm);
     Serial.print(" cm");
     Serial.println();
+    // change it to an integer to make things simple
+    distance = int(cm);
+    return distance;
     // delay for stability
     delay(100);
 }
 
-void playMusic() {  
+// our main loop
 
+void loop() {
+    int distance;
+    // do a ping to get the distance to sensor, whatever it returns call it distance
+    distance = ping();
+    // if the returned ping() value is high enough, turn on the LED and playMusic()
+    if (distance > threshold) {
+        digitalWrite(ledPin, HIGH);
+        Serial.print("Distance over threshold so play music")
+        Serial.println();
+        playMusic();
+        // dont bother otherwise & set LED to off
+        } else {
+            Serial.print("You're not close enough! So not playing the file");
+            digitalWrite(ledPin, LOW);
+            }
+        delay(10);        // delay in between reads for stability
+        }
+
+// here's our play music function, void because we dont need to return anything
+
+void playMusic() {  
+  int latestDistance;
   // Start playing a file, then we can do stuff while waiting for it to finish
   if (! musicPlayer.startPlayingFile("/track001.mp3")) {
-    Serial.println("Could not open file track001.mp3");
+    Serial.println("Could not open file track001.mp3"); // error message if we find musicPlayer NOT (!) playing 
     while (1);
   }
   Serial.println(F("Started playing"));
 
   while (musicPlayer.playingMusic) {
     // file is now playing in the 'background' so now's a good time
-    // to do something else like handling our sensor :)
+    // to do something else like handling our sensor and doing a PING :)
     Serial.print(".");
-    long distance = ping();
-
-    // Old analog method
-    // OK lets check for reading from the echo pin on our sensor
-    // read the input on analog pin 0: hopefully this is an ok pin
-    // if not we can try pins A1-4
-    // int sensorValue = analogRead(analogPin);
-    // Convert the analog reading (which probably goes from 0 - 1023) to a voltage (0 - 5V):
-    //float voltage = sensorValue * (5.0 / 1023.0);
-    // print out the value you read:
-    Serial.println("Distance is...");
-    Serial.println(distance);
-    // really roughly take the voltage off the volume value, we can
-    // tweak this and follow the sensor guides to estimate distance
-    // but this should get things started
-    // float newVolume = (20 - voltage);
-    //
-    //
-    long newVolume = startVolume - distance;
+    latestDistance = ping();
+    Serial.println("Distance is now...");
+    Serial.println(latestDistance);
+    // Add the distance value to the volume value because lower values mean louder sound
+    // so big distance == bigger volume value == lower actual volume
+    // tweak this if its too extreme
+    int newVolume = startVolume + latestDistance;
     Serial.println("Volume is now...");
     Serial.println(newVolume);
     musicPlayer.setVolume(newVolume,newVolume);
-    delay(500);
+    delay(500); // delay for stability
+    // this loops as long as musicPlayer is playingMusic
     }
     Serial.println("Done playing music");
     }
@@ -216,7 +199,6 @@ void playMusic() {
 /// File listing helper
 void printDirectory(File dir, int numTabs) {
    while(true) {
-     
      File entry =  dir.openNextFile();
      if (! entry) {
        // no more files
